d4e73871fd14fd1338eee1547583ae5d44efddac
Michael Fearn <fearn.michael@gmail.com>
2020-05-04 11:02:26 -0600

    thisAmount => result

--- theatrical-company/index.ts
+++ theatrical-company/index.ts
@@ -1,17 +1,43 @@
 import invoicesJson from "./invoices.json";
 import playsJson from "./plays.json";
 
+type Performance = { playID: string; audience: number };
+
 type Invoice = {
   customer: string;
-  performances: { playID: string; audience: number }[];
+  performances: Performance[];
+};
+
+type Play = {
+  name: string;
+  type: string;
 };
 
 type Plays = {
-  [key: string]: {
-    name: string;
-    type: string;
-  };
+  [key: string]: Play;
 };
+
+function amountFor(perf: Performance, play: Play) {
+  let result = 0;
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20);
+      }
+      result += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return result;
+}
 
 export function statement(invoice: Invoice, plays: Plays) {
   let totalAmount = 0;
@@ -25,24 +51,7 @@ export function statement(invoice: Invoice, plays: Plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    const thisAmount = amountFor(perf, play);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
