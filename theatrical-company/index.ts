e5b103007d23c23c90cbdff2d891cfb9fb313a98
Michael Fearn <fearn.michael@gmail.com>
2020-05-04 11:10:29 -0600

    replaced temp with query

--- theatrical-company/index.ts
+++ theatrical-company/index.ts
@@ -1,5 +1,5 @@
-import invoicesJson from "./invoices.json";
-import playsJson from "./plays.json";
+import invoicesRaw from "./invoices.json";
+import playsRaw from "./plays.json";
 
 type Performance = { playID: string; audience: number };
 
@@ -17,26 +17,33 @@ type Plays = {
   [key: string]: Play;
 };
 
-function amountFor(perf: Performance, play: Play) {
+const invoices = invoicesRaw as Invoice[];
+const plays = playsRaw as Plays;
+
+function amountFor(aPerformance: Performance, play: Play) {
   let result = 0;
   switch (play.type) {
     case "tragedy":
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
   return result;
+}
+
+function playFor(aPerformance: Performance) {
+  return plays[aPerformance.playID];
 }
 
 export function statement(invoice: Invoice, plays: Plays) {
@@ -50,7 +57,7 @@ export function statement(invoice: Invoice, plays: Plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     const thisAmount = amountFor(perf, play);
 
     // add volume credits
