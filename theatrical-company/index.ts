a3f9f73445b3ed35d4c67733bf067e8183e98838
Michael Fearn <fearn.michael@gmail.com>
2020-05-04 11:54:46 -0600

    makes statement top level function

--- theatrical-company/index.ts
+++ theatrical-company/index.ts
@@ -20,66 +20,6 @@ type Plays = {
 const invoices = invoicesRaw as Invoice[];
 const plays = playsRaw as Plays;
 
-function usd(aNumber: number) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumIntegerDigits: 2,
-  }).format(aNumber / 100);
-}
-
-function amountFor(aPerformance: Performance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-  }
-  return result;
-}
-
-function playFor(aPerformance: Performance) {
-  return plays[aPerformance.playID];
-}
-
-function volumeCreditsFor(aPerformance: Performance) {
-  let results = 0;
-  results += Math.max(aPerformance.audience - 30, 0);
-  // add extra credit for every ten comedy attendees
-  if ("comedy" === playFor(aPerformance).type)
-    results += Math.floor(aPerformance.audience / 5);
-
-  return results;
-}
-
-function totalVolumeCredits(invoice: Invoice) {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-}
-
-function totalAmount(invoice: Invoice) {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
 export function statement(invoice: Invoice) {
   let result = `Statement for ${invoice.customer}\n`;
 
@@ -90,9 +30,69 @@ export function statement(invoice: Invoice) {
     } seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
-  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function usd(aNumber: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumIntegerDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function amountFor(aPerformance: Performance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function playFor(aPerformance: Performance) {
+    return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(aPerformance: Performance) {
+    let results = 0;
+    results += Math.max(aPerformance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === playFor(aPerformance).type)
+      results += Math.floor(aPerformance.audience / 5);
+
+    return results;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 }
 // for (let invoice of invoicesJson) {
 //   console.log(statement(invoice, playsJson));
