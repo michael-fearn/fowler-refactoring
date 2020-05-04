b75c976c106b23a8da1ee4bee67e73cfbdeccecc
Michael Fearn <fearn.michael@gmail.com>
2020-05-04 11:39:22 -0600

    replace temp with query, extact function

--- theatrical-company/index.ts
+++ theatrical-company/index.ts
@@ -66,7 +66,6 @@ function usd(aNumber: number) {
 
 export function statement(invoice: Invoice) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -76,10 +75,15 @@ export function statement(invoice: Invoice) {
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
 
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
